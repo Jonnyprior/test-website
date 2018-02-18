a665cb26d4c843a2e7971fdf685601f6b917fb2d
Jonny-PC\Jonny <jonnyprior@hotmail.com>
2018-02-18 21:40:46 +0000

    Added bootstrap template to tutorial

--- tutorial/views.py
+++ tutorial/views.py
@@ -9,7 +9,7 @@ def fooditem(request):
 	table = FoodItemsTable(FoodItems.objects.all())
 	RequestConfig(request).configure(table)
 	return render(request, 'tutorial/fooditem.html', {'table': table})
-	
+
 	#http://django-datatable-view.appspot.com/x-editable-columns/
 	#https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Forms
 	#https://elo80ka.wordpress.com/2009/10/10/jquery-plugin-django-dynamic-formset/
